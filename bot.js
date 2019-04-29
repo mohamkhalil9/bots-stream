09244b42f81d9abf8906c324cc1c4a339c719852
II_Snax_II <45282148+mohamkhalil9@users.noreply.github.com>
2019-04-29 15:14:09 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -5,7 +5,7 @@ client.on('ready', () => {
 });
 
 
-const developers = ["415142691282616330"]
+const developers = ["448900716220907531"]
 const adminprefix = "!";
 client.on('message', message => {
     var argresult = message.content.split(` `).slice(1).join(' ');
